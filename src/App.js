abe04e562a5e274f053374f28e24597339326165
ericmed1 <eric_ivan1@yahoo.com>
2024-09-07 16:36:22 -0500

    home screen
    added more elements tp the home screen to make more asthetic

--- src/App.js
+++ src/App.js
@@ -9,12 +9,19 @@ function App() {
           WorkoutBuddie
         </h1>
         <img src="/images/WorkoutBuddie.png" className="App-logo" alt="logo" />
-        <p>
-          This is WorkoutBuddie a fitness tracker with a built in 
-          precentage calulator to simplify your gym life!
-        </p>
         
       </header>
+      <div className = "App-info">
+        <h1 className='welcome'>
+          Welcome to WorkoutBuddie! 
+        </h1>
+        <p className = "pinfo">
+          Workbuddie is a workout tracker that will help you not only plan out your workout but also help 
+          you know what wieght you should be doing for whatever lift you may be doing. Our main goal is to 
+          help you along your wonderful fitness journey and hit all your goals!
+        </p>
+        <img src ="/images/1.png" classname="logo2"/>
+      </div>
     </div>
   );
 }
