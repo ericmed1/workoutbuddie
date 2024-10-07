bc43405bc82df4bad425ee27a20257936ae8ca19
ericmed1 <eric_ivan1@yahoo.com>
2024-10-07 17:14:47 -0500

    Update Workout-log.js
    added place holder

--- src/components/Workout-log.js
+++ src/components/Workout-log.js
@@ -5,25 +5,27 @@ import { useState } from 'react';
 
 function Workoutlog(){
     const [workouts, setWorkouts] = useState([])
-    const [workoutname, setWorkoutname] = useState("Enter workout name")
-    const [sets,setSets] = useState(0)
-    const [reps,setReps] = useState(0)
-    const [max, setMax] = useState("0")
+    const [workoutname, setWorkoutname] = useState()
+    const [sets,setSets] = useState()
+    const [reps,setReps] = useState()
+    const [weight, setWeight] = useState()
     // this event handler makes it so when the user click the add workout button it will then call this function
     // and this function will take in the info the user inputed and if everything is inputed the function
     // will make a newworkout with all the needed fields and then will add the newworkout into the 
     // workouts array and add that new row into the table
     const handleAddWorkout = () => {
-        if(workoutname && sets > 0 && reps > 0){
+        if(workoutname && sets > 0 && reps > 0 && weight > 0){
             const newWorkout = {
                 workoutname,
                 sets,
-                reps
+                reps,
+                weight
             };
             setWorkouts([...workouts, newWorkout]);
             setWorkoutname("Enter Workout name");
             setReps(0);
             setSets(0);
+            setWeight(0);
 
         }
     }
@@ -43,17 +45,26 @@ function Workoutlog(){
                     type = "text"
                     value={workoutname}
                     onChange={(e) => setWorkoutname(e.target.value)} 
+                    placeholder="Enter Workout Name"
                 /> 
                 <input
                     type = "number"
                     value={sets}
                     onChange={(e) => setSets(parseInt(e.target.value, 10))}
+                    placeholder="Enter Sets"
                 />
                 <input
                     type = "number"
                     value={reps}
                     onChange={(e) => setReps(parseInt(e.target.value, 10))}
+                    placeholder="Enter Reps"
                 />
+                <input
+                    type = "number"
+                    value ={weight}
+                    onChange={(e) => setWeight(parseInt(e.target.value, 10))}
+                    placeholder="Enter Weight"
+                    />
                 <button onClick={handleAddWorkout}>Add</button>
             </div>
 
@@ -63,6 +74,7 @@ function Workoutlog(){
                         <th>Workout Name</th>
                         <th>Sets</th>
                         <th>Reps</th>
+                        <th>Weight</th>
                         <th></th>
                     </tr>
                 </thead>
@@ -72,6 +84,7 @@ function Workoutlog(){
                             <td>{workout.workoutname}</td>
                             <td>{workout.sets}</td>
                             <td>{workout.reps}</td>
+                            <td>{workout.weight}</td>
                             <td>
                                 <button onClick={() => handleDelete(index)}>Delete</button>
                             </td>
